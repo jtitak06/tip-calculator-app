679806f5a5c4c9d32f36fdef517591f52bf3d537
Jeff Titak <jtitak06@yahoo.com>
2021-08-27 15:55:11 -0500

    First commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+const GridButtons = document.querySelectorAll('.tip-calculator__grid-button');
+const GridInput = document.querySelector('.tip-calculator__grid-input');
+const TipAmount = document.querySelector('.tip-calculator__input_type_tip-amount');
+const TotalAmount = document.querySelector('.tip-calculator__input_type_total');
+const TipDollars = document.querySelector('.tip-calculator__input_type_dollars');
+const TotalPeople = document.querySelector('.tip-calculator__input_type_people');
+
+// Calculate tip amount / person with grid buttons
+function CalculateTip() {
+TotalAmount.textContent = (GridButtons.value * TipAmount.value) / TotalPeople.value;
+console.log(TotalAmount.textContent);
+}
+
